5806378a1008fac15d372ae1f331445e98fe9159
Ed Spencer <ed@edspencer.net>
2024-10-03 15:25:21 -0400

    docs

--- packages/react/src/Provider.tsx
+++ packages/react/src/Provider.tsx
@@ -4,6 +4,21 @@ type CreateNarratorProps = {
   actions?: any;
 };
 
+/**
+ * Creates a Narrator component that provides narration context to its children. Example usage:
+ *
+ * const NarratorProvider = createNarrator({
+ *   actions: {
+ *     saveExample,
+ *     regenerateNarration,
+ *   }
+ * });
+ *
+ * @param {CreateNarratorProps} [params={}] - The parameters for creating the Narrator.
+ * @param {object} [params.actions={}] - An object containing actions to be provided by the NarrationProvider.
+ *
+ * @returns {React.FC<{ children: React.ReactNode }>} A React functional component that wraps its children with a NarrationProvider.
+ */
 export function createNarrator(params: CreateNarratorProps = {}) {
   const { actions = {} } = params;
 
--- packages/react/src/useNarration.tsx
+++ packages/react/src/useNarration.tsx
@@ -2,6 +2,16 @@
 
 import { useNarrationContext } from "./Context";
 
+/**
+ * Custom hook that provides access to the narration context. This provides
+ * access to functions like `isLoading`, `saveExample`, and `regenerate`.
+ *
+ * This hook must be used within a `NarrationProvider`. If it is used outside
+ * of a `NarrationProvider`, it will throw an error.
+ *
+ * @returns {NarrationContext} The narration context.
+ * @throws {Error} If the hook is used outside of a `NarrationProvider`.
+ */
 export function useNarration() {
   const context = useNarrationContext();
   if (!context) {
